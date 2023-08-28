fa13a71c1e02309201bb90c19d26a3c12d045802
mielmielamayel <143404324+mielmielamayel@users.noreply.github.com>
2023-08-28 20:00:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let clickCount = 5; // Initialize the click count
+
+document.getElementById("heart")?.addEventListener("click", () => {
+  if (clickCount > 0) {
+    const clickInstructions = document.getElementById("click-instructions");
+    
+    if (clickCount === 1) {
+      clickInstructions.textContent = "";
+      document.getElementById("heart").style.animation = "explode 1s forwards";
+      
+      setTimeout(() => {
+        const explosionText = document.createElement("p");
+        explosionText.textContent = "I MISS YOU ELISHA";
+        explosionText.className = "explosion-text";
+        document.querySelector(".page").appendChild(explosionText);
+        
+        setTimeout(() => {
+          window.location.href = "page3.html";
+        }, 2000);
+      }, 700); // Add a delay for the "I love you" text
+    } else {
+      clickInstructions.textContent = `Click this heart ${clickCount - 1} times`;
+      clickCount--;
+    }
+  }
+});
+
+document.getElementById("proceed-button")?.addEventListener("click", () => {
+  window.location.href = "page2.html"; // Change the page URL accordingly
+});
+
+document.getElementById("back-button")?.addEventListener("click", () => {
+  window.history.back();
+});
